e2d9a1f4a337144480c2798f0685528dc92b6774
shobarna bhuiya <shoma@shobarnas-MacBook-Pro.local>
2026-04-13 17:12:35 +0600

    api2

---
+++ src/Pages/ApiTest2.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect } from "react";
+// async, await
+
+// Axios
+
+//const Api = () => {
+  //useEffect(() => {
+    // console.log("Hello 1");
+
+    // setTimeout(() => {
+    //   console.log("Hello from timeout 1");
+    // }, 1000);
+    
+    // setTimeout(() => {
+    //   console.log("Hello from timeout 2");
+    // }, 0);
+
+    // console.log("Hello 2");
+  //}, []);
+  //const Api = () => {
+  //useEffect(() => {
+    //const api = async () => {
+      //const data = await fetch("https://jsonplaceholder.typicode.com/users");
+      //const res = await data.json();
+     // console.log(res);
+   // };
+   // api();
+  //}, []);
+
+
+  //return <div>Api</div>;
+//};
+// async, await
+
+//const Api = () => {
+  //useEffect(() => {
+    //const api = async () => {
+      //const data = await fetch("https://jsonplaceholder.typicode.com/users");
+      //const res = await data.json();
+      //console.log(res);
+   // };
+    //api();
+ // }, []);
+
+  //useEffect(() => {
+    //(async () => {
+      //const data = await axios.get(
+       // "https://jsonplaceholder.typicode.com/users"
+     // );
+     // console.log(data.data);
+   // })();
+ // }, []);
+
+  //return <div>Api</div>;
+//};
+
+
+
+export default Api;
